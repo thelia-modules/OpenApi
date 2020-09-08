0aa52cef68cbd6670e7f0d9eb5b6d9640a727a35
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2020-09-08 09:52:40 +0200

    Add Search and results models

---
+++ Model/Api/Result.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace OpenApi\Model\Api;
+
+/**
+ * Class Result
+ * @package OpenApi\Model\Api
+ * @OA\Schema(
+ *     description="A result (to be in search object)"
+ * )
+ */
+class Result extends BaseApiModel
+{
+    /**
+     * @var float
+     * @OA\Property(
+     *     type="number",
+     *     format="float",
+     *     description="The weight of result in search"
+     * )
+     */
+    protected $weight;
+
+    /**
+     * @var string
+     * @OA\Property(
+     *     type="string"
+     * )
+     */
+    protected $type;
+
+    /**
+     * @var BaseApiModel
+     * @OA\Property(
+     *     type="object",
+     *     description="Almost any of OpenApi object"
+     * )
+     */
+    protected $object;
+
+    /**
+     * @return float
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param float $weight
+     *
+     * @return Result
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return Result
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return BaseApiModel
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * @param BaseApiModel $object
+     *
+     * @return Result
+     */
+    public function setObject($object)
+    {
+        $this->object = $object;
+        return $this;
+    }
+}
---
+++ Model/Api/Search.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace OpenApi\Model\Api;
+
+/**
+ * Class Search
+ * @package OpenApi\Model\Api
+ * @OA\Schema(
+ *     description="A search response"
+ * )
+ */
+class Search extends BaseApiModel
+{
+    /**
+     * @var Result
+     * @OA\Property(
+     *     type="array",
+     *     @OA\Items(
+     *          ref="#/components/schemas/Result"
+     *     )
+     * )
+     */
+    protected $results;
+
+    /**
+     * @var integer
+     * @OA\Property(
+     *     type="number",
+     *     format="integer"
+     * )
+     */
+    protected $page;
+
+    /**
+     * @var integer
+     * @OA\Property(
+     *     type="number",
+     *     format="integer",
+     *     description="The total of results for current page"
+     * )
+     */
+    protected $pageTotal;
+
+    /**
+     * @var integer
+     * @OA\Property(
+     *     type="number",
+     *     format="integer",
+     *     description="The whole total of results (without paging)"
+     * )
+     */
+    protected $total;
+
+    /**
+     * @return Result
+     */
+    public function getResults()
+    {
+        return $this->results;
+    }
+
+    /**
+     * @param Result $results
+     *
+     * @return Search
+     */
+    public function setResults($results)
+    {
+        $this->results = $results;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param int $page
+     *
+     * @return Search
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPageTotal()
+    {
+        return $this->pageTotal;
+    }
+
+    /**
+     * @param int $pageTotal
+     *
+     * @return Search
+     */
+    public function setPageTotal($pageTotal)
+    {
+        $this->pageTotal = $pageTotal;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * @param int $total
+     *
+     * @return Search
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+        return $this;
+    }
+}

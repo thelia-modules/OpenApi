baf48b7f9a8dc1f3c57d2551240b1fc172e99f5f
Damien Foulhoux <Lucanis@users.noreply.github.com>
2020-11-05 16:29:59 +0100

    Update fr_FR.php

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -2,5 +2,6 @@
 
 return array(
     'This value should not be blank' => 'Cette valeur ne peut pas être vide',
-    'No customer found for this email.' => 'Aucun compte client correspondant'
+    'No customer found for this email.' => 'Aucun compte client correspondant',
+    'No coupons were found for this coupon code.' => 'Aucun code promo correspondant'
 );

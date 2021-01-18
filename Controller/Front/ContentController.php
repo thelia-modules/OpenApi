b16832d4258af6be33ca21d612f6b0c5973d87a9
adrien <adrien2go@libratoi.fr>
2021-01-18 12:16:10 +0100

    Dev (#17)
    * Add get content route
    
    Co-authored-by: aberard <aberard@openstudio.fr>

---
+++ Controller/Front/ContentController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OpenApi\Controller\Front;
+
+use Exception;
+use OpenApi\OpenApi;
+use Thelia\Core\HttpFoundation\JsonResponse;
+use Thelia\Core\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
+use Thelia\Core\Translation\Translator;
+use Thelia\Model\ContentQuery;
+
+/**
+ * @Route("/content", name="content")
+ */
+class ContentController extends BaseFrontOpenApiController
+{
+    /**
+     * @Route("/{id}", name="get_content", methods="GET")
+     *
+     * @OA\Get(
+     *     path="/content/{id}",
+     *     tags={"content"},
+     *     summary="Get content values by ID",
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          example="1",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response="200",
+     *          description="Success",
+     *          @OA\JsonContent(ref="#/components/schemas/Content")
+     *     ),
+     *     @OA\Response(
+     *          response="400",
+     *          description="Bad request",
+     *          @OA\JsonContent(ref="#/components/schemas/Error")
+     *     )
+     * )
+     * @throws Exception
+     */
+    public function getContent(Request $request, $id)
+    {
+        $modelFactory = $this->getModelFactory();
+        $content = ContentQuery::create()
+            ->findOneById($id);
+        $apiContent = $modelFactory->buildModel('Content', $content);
+
+        if (null === $content) {
+            throw new Exception(Translator::getInstance()->trans("Content does not exist.", [], OpenApi::DOMAIN_NAME));
+        }
+
+        return new JsonResponse($apiContent);
+    }
+}
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -3,5 +3,6 @@
 return array(
     'This value should not be blank' => 'Cette valeur ne peut pas être vide',
     'No customer found for this email.' => 'Aucun compte client correspondant',
-    'No coupons were found for this coupon code.' => 'Aucun code promo correspondant'
+    'No coupons were found for this coupon code.' => 'Aucun code promo correspondant',
+    'Content does not exist.' => "Le contenu demandé n'éxiste pas."
 );

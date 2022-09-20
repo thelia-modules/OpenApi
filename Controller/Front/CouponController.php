926bf96562389e732cd75309bb55af251188c7d1
Mathis Chambon <matchambon60@gmail.com>
2022-09-20 12:01:34 +0200

    Added delete route for a specific coupon in the cart

--- Controller/Front/CouponController.php
+++ Controller/Front/CouponController.php
@@ -2,6 +2,7 @@
 
 namespace OpenApi\Controller\Front;
 
+use Exception;
 use OpenApi\Annotations as OA;
 use OpenApi\Model\Api\Coupon;
 use OpenApi\Model\Api\ModelFactory;
@@ -13,6 +14,7 @@ use Symfony\Contracts\EventDispatcher\Event;
 use Thelia\Core\Event\Coupon\CouponConsumeEvent;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\HttpFoundation\Request;
+use Thelia\Core\HttpFoundation\Session\Session;
 use Thelia\Core\Translation\Translator;
 use Thelia\Exception\UnmatchableConditionException;
 use Thelia\Model\CouponQuery;
@@ -116,6 +118,61 @@ class CouponController extends BaseFrontOpenApiController
             $dispatcher->dispatch((new Event()), TheliaEvents::COUPON_CLEAR_ALL);
         } catch (\Exception $exception) {
             throw new \Exception(Translator::getInstance()->trans('An error occurred while clearing coupons : ') . $exception->getMessage());
+        }
+
+        return OpenApiService::jsonResponse($modelFactory->buildModel('Cart', $cart));
+    }
+
+    /**
+     * @Route("/clear/{id}", name="clear_coupon", methods="GET")
+     *
+     * @OA\Get(
+     *     path="/coupon/clear/{id}",
+     *     tags={"coupon"},
+     *     summary="Clear a specific coupon from cart"
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          example="1",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response="200",
+     *          description="Success",
+     *          @OA\JsonCocalontent(ref="#/components/schemas/Cart")
+     *     ),
+     *     @OA\Response(
+     *          response="400",
+     *          description="Bad request",
+     *          @OA\JsonContent(ref="#/components/schemas/Error")
+     *     )
+     * )
+     */
+    public function clearCoupon(
+        EventDispatcherInterface $dispatcher,
+        ModelFactory $modelFactory,
+        Session $session,
+        $id
+    ) {
+        $cart = $session->getSessionCart($dispatcher);
+
+        try {
+            $coupon = CouponQuery::create()->findOneById($id);
+
+            if (null === $coupon) {
+                throw new Exception();
+            }
+
+            $consumedCoupons = $session->getConsumedCoupons();
+
+            unset($consumedCoupons[$coupon->getCode()]);
+
+            $session->setConsumedCoupons($consumedCoupons);
+        } catch (Exception $e) {
+            throw new Exception(Translator::getInstance()->trans('An error occurred while clearing coupon ' . $id . ' : ') . $e->getMessage());
         }
 
         return OpenApiService::jsonResponse($modelFactory->buildModel('Cart', $cart));

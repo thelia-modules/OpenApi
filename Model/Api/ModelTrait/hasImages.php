3c336808b99bf3dac5eed076b347c28fd8cdc99b
nbarbey-os-alter <nbarbey@openstudio.fr>
2023-02-21 14:45:54 +0100

    fix image trait

---
+++ Model/Api/ModelTrait/hasImages.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OpenApi\Model\Api\ModelTrait;
+
+
+/**
+ * @OA\Schema
+ * Trait translatable
+ */
+trait hasImages
+{
+    /**
+     * @var array
+     * @OA\Property(
+     *    type="array",
+     *     @OA\Items(
+     *          ref="#/components/schemas/File"
+     *     )
+     * )
+     */
+    protected $images = [];
+
+    /**
+     * @return array
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
+
+    /**
+     * @param array $images
+     *
+     * @return $this
+     */
+    public function setImages($images)
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+
+    public function createImagesFromTheliaModel()
+    {
+        usort($this->images, function ($item1, $item2) {
+            return $item1->getPosition() <=> $item2->getPosition();
+        });
+    }
+}
